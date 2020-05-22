6cf4b0ba1d6e0547a679972b86303c9ef901e768
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-22 12:01:53 +0200

    Añadida la nueva ruta al programa principal

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 key=os.environ["clave_propia"]
 #payload = {"locale=es_ES":"es_ES"}
 headers={"Authorization":"bearer "+key}
-URL_BASE_CARTAS = "https://us.api.blizzard.com/hearthstone/cards"
+URL_BASE_CARTAS = "https://us.api.blizzard.com/hearthstone/cards?locale=es_ES"
 #URL_BASE_PARAMETROS = "https://us.api.blizzard.com/hearthstone/metadata/"
 app=Flask(__name__)
 
@@ -14,6 +14,19 @@ app=Flask(__name__)
 @app.route('/', methods=["GET"])
 def inicio():
     return render_template('inicio.html')
+
+
+@app.route('/todas_cartas/<int:pagina>', methods=["GET"])
+def cartas(pagina):
+    peticion_todas_cartas=requests.get(URL_BASE_CARTAS+'&pageSize=21&page='+str(pagina), headers=headers)
+    if peticion_todas_cartas.status_code == 200:
+        doc_cartas=peticion_todas_cartas.json()
+        return render_template('todas-cartas.html', doc_cartas=doc_cartas, pagina=1)
+    else:
+        return abort(404)
+
+
+
 
 app.run('0.0.0.0', debug=True)
 
